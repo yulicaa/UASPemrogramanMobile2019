6c7b243d0a0944b02b9f1fe89aeef0f892b1d081
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2019-12-24 15:24:04 +0700

    Menambah Splash Activity

---
+++ app/src/main/java/com/yulica/uaspemrogramanmobile2019/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yulica.uaspemrogramanmobile2019.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.yulica.uaspemrogramanmobile2019.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
