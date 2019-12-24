f98e84ff9f0ed49f966083658af8d06de6aba375
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2019-12-24 17:03:54 +0700

    Menambah login activity

---
+++ UseeApp/app/src/main/java/com/yulica/useeapp/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.yulica.useeapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yulica.useeapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
